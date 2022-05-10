9e3334ecab02d641ca7fca4e42e2014b36fd83ea
Hussein Lababidi <husseincak@hotmail.com>
2022-05-10 15:09:39 +0200

    Página Contador

--- src/pages/ViewCountdown.jsx
+++ src/pages/ViewCountdown.jsx
@@ -1,5 +1,23 @@
+import { useSearchParams } from 'react-router-dom';
+import Countdown from '../components/Countdown';
+
 const ViewCountdown = () => {
-	return <div>ViewCountdown</div>;
+	const [searchParams] = useSearchParams();
+
+	const year = searchParams.get('year');
+	const month = searchParams.get('month') && searchParams.get('month') - 1;
+	const day = searchParams.get('day');
+	const hour = searchParams.get('hour');
+	const minute = searchParams.get('minute');
+	const second = searchParams.get('second');
+
+	const date = new Date(year, month, day, hour, minute, second, 0);
+
+	console.log('date', date);
+	console.log('month', month);
+	console.log('day', day);
+
+	return <main>{<Countdown date={date} />}</main>;
 };
 
 export default ViewCountdown;
